99a5aeaeddaf3971fd550614eae1b9d0aed019f9
xue-fei <awnuxcvbn@163.com>
2023-08-23 15:06:19 +0800

    初步实现功能

---
+++ Assets/Scripts/MainPanel.cs
@@ -0,0 +1,129 @@
+﻿using System.Net;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainPanel : MonoBehaviour
+{
+    private UdpServer server;
+
+    public TextMeshProUGUI txtLocIP;
+    public TMP_InputField ifDstIP;
+    public TMP_InputField ifDstPort;
+    public TMP_InputField ifLocPort;
+    public Button btnAdd;
+    public Button btnClose;
+    public TMP_InputField ifSend;
+    public Button btnSend;
+    public TMP_InputField ifReceive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Loom.Initialize();
+
+        btnClose.gameObject.SetActive(false);
+
+        txtLocIP.text = "本机IP:" + GetLocalIP();
+        btnAdd.onClick.AddListener(() =>
+        {
+            BtnClick(btnAdd);
+        });
+        btnClose.onClick.AddListener(() =>
+        {
+            BtnClick(btnClose);
+        });
+        btnSend.onClick.AddListener(() =>
+        {
+            BtnClick(btnSend);
+        });
+        EventCenter.AddListener<string>("Receive", OnMessage);
+        EventCenter.AddListener<string>("Error", OnError);
+    }
+
+    void BtnClick(Button button)
+    {
+        if (button.name == btnAdd.name)
+        {
+            if (!string.IsNullOrEmpty(ifDstIP.text)
+                && !string.IsNullOrEmpty(ifDstPort.text)
+                && !string.IsNullOrEmpty(ifLocPort.text))
+            {
+                server = new UdpServer();
+                int locPort = 0;
+                int.TryParse(ifLocPort.text, out locPort);
+                int dstPort = 0;
+                int.TryParse(ifDstPort.text, out dstPort);
+
+                server.Start(locPort, ifDstIP.text, dstPort);
+
+                ifDstIP.interactable = false;
+                ifDstPort.interactable = false;
+                ifLocPort.interactable = false;
+            }
+            btnAdd.gameObject.SetActive(false);
+            btnClose.gameObject.SetActive(true);
+        }
+        if (button.name == btnClose.name)
+        {
+            server.Dispose();
+            server = null;
+            btnClose.gameObject.SetActive(false);
+            btnAdd.gameObject.SetActive(true);
+
+            ifDstIP.interactable = true;
+            ifDstPort.interactable = true;
+            ifLocPort.interactable = true;
+        }
+        if (button.name == btnSend.name)
+        {
+            if (server != null && !string.IsNullOrEmpty(ifSend.text))
+            {
+                server.Send(ifSend.text);
+            }
+        }
+    }
+
+    void OnMessage(string msg)
+    {
+        Loom.QueueOnMainThread(() =>
+        {
+            ifReceive.text += msg+ "\r\n";
+        });
+    }
+
+    void OnError(string error)
+    {
+        Loom.QueueOnMainThread(() =>
+        {
+            ifReceive.text += error+ "\r\n";
+        });
+    }
+
+    private void OnApplicationQuit()
+    {
+        EventCenter.RemoveListener<string>("Receive", OnMessage);
+        EventCenter.RemoveListener<string>("Error", OnError);
+    }
+
+    /// <summary>
+    /// 获取本机ip
+    /// </summary>
+    /// <returns></returns>
+    string GetLocalIP()
+    {
+        string strHostName = Dns.GetHostName();
+        IPHostEntry ipEntry = Dns.GetHostEntry(strHostName);
+        IPAddress[] addr = ipEntry.AddressList;
+        string ip = "";
+        foreach (IPAddress ipd in addr)
+        {
+            if (ipd.ToString().Contains("."))
+            {
+                ip += "      " + ipd;
+            }
+        }
+        return ip;
+    }
+}
---
+++ Assets/Scripts/UdpServer.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+
+public class UdpServer : IDisposable
+{
+    Socket socket;
+    EndPoint epDst;
+    byte[] bufferData = new byte[1024];
+
+    // Start is called before the first frame update
+    public void Start(int localPort, string dstIP, int dstPort)
+    {
+        try
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            socket.Bind(new IPEndPoint(IPAddress.Any, localPort));
+
+            epDst = (EndPoint)new IPEndPoint(IPAddress.Parse(dstIP), dstPort);
+
+            //开始异步接收数据
+            socket.BeginReceive(bufferData, 0, bufferData.Length,
+                SocketFlags.None, new AsyncCallback(ReceiveData), null);
+
+            Debug.Log("Pad UdpServer启动成功 端口号:" + localPort);
+        }
+        catch (Exception e)
+        {
+            EventCenter.Broadcast("Error", e.ToString());
+            Debug.LogError("UdpServer启动失败 端口号:" + localPort);
+            Debug.LogError(e.ToString());
+        }
+    }
+
+    /// <summary>
+    /// 接收消息并存放 
+    /// </summary>
+    /// <param name="iar"></param>
+    private void ReceiveData(IAsyncResult iar)
+    {
+        try
+        {
+            if (socket != null)
+            {
+                int length = socket.EndReceive(iar);
+                //继续监听
+                socket.BeginReceive(bufferData, 0, bufferData.Length,
+                    SocketFlags.None, new AsyncCallback(ReceiveData), null);
+
+                if (length > 0)
+                {
+                    byte[] data = new byte[length];
+                    Buffer.BlockCopy(bufferData, 0, data, 0, length);
+                    string recvStr = Encoding.UTF8.GetString(data, 0, data.Length);
+                    EventCenter.Broadcast("Receive", recvStr);
+                    Debug.Log($"收到PC端发过来的数据:{recvStr}");
+                }
+            }
+            else
+            {
+                Debug.LogError("serverSocket == null");
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("接收消息出错");
+            Debug.LogException(e);
+        }
+    }
+
+    public void Send(string sendStr)
+    {
+        try
+        {
+            //发送给指定客户端
+            byte[] sendData = Encoding.UTF8.GetBytes(sendStr);
+            socket.SendTo(sendData, epDst);
+        }
+        catch(Exception e)
+        {
+            EventCenter.Broadcast("Error", e.ToString());
+        }
+    }
+
+    public void Dispose()
+    {
+        // 关闭socket
+        if (socket != null)
+        {
+            try
+            {
+                socket.Close();
+                socket.Dispose();
+                socket = null;
+                GC.Collect();
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+            }
+        }
+        epDst = null;
+
+        Debug.Log("关闭server");
+    }
+}
